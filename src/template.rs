cc9b18c9e14b48f7548244c4d24948a11197cccd
SuniRein <sunirein@qq.com>
2025-05-04 16:44:15 +0800

    test: add template::test_read_from_path

--- src/template.rs
+++ src/template.rs
@@ -25,7 +25,7 @@ impl Template {
         &self.meta.description
     }
 
-    pub fn location(&self) -> &PathBuf {
+    pub fn location(&self) -> &Path {
         &self.location
     }
 
@@ -51,5 +51,69 @@ impl Template {
             location: path.to_path_buf(),
             meta,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use hamcrest2::prelude::*;
+
+    mod test_read_from_path {
+        use super::*;
+
+        fn create_template_dir(name: &str) -> (tempfile::TempDir, PathBuf) {
+            let temp_dir = tempfile::tempdir().unwrap();
+            let template_dir = temp_dir.path().join(name);
+            fs::create_dir(&template_dir).unwrap();
+            (temp_dir, template_dir)
+        }
+
+        fn create_meta_file(dir: &Path, content: &str) {
+            let meta_file = dir.join(Template::META_FILE);
+            fs::write(&meta_file, content).unwrap();
+        }
+
+        #[test]
+        fn path_invalid() {
+            assert_that!(
+                Template::read_from_path(&PathBuf::from("/invalid/path/..")),
+                err()
+            );
+        }
+
+        #[test]
+        fn meta_file_missing() {
+            let (_temp_dir, template_dir) = create_template_dir("test template");
+            assert_that!(Template::read_from_path(&template_dir), err());
+        }
+
+        #[test]
+        fn meta_file_is_invalid_toml() {
+            let (_temp_dir, template_dir) = create_template_dir("test template");
+            create_meta_file(&template_dir, "invalid toml content");
+
+            assert_that!(Template::read_from_path(&template_dir), err());
+        }
+
+        #[test]
+        fn meta_file_missing_description_field() {
+            let (_temp_dir, template_dir) = create_template_dir("template");
+            create_meta_file(&template_dir, "title = \"no description\"");
+            assert_that!(Template::read_from_path(&template_dir), err());
+        }
+
+        #[test]
+        fn valid_template() {
+            let (_temp_dir, template_dir) = create_template_dir("test template");
+            create_meta_file(&template_dir, "description = \"Test template\"");
+
+            let template = Template::read_from_path(&template_dir).unwrap();
+
+            assert_eq!(template.name(), "test template");
+            assert_eq!(template.description(), "Test template");
+            assert_eq!(template.location(), template_dir);
+        }
     }
 }
