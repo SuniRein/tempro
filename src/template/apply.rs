afa32c749c975f7798903a05d2f691b5e5966cfd
SuniRein <sunirein@qq.com>
2025-05-06 12:52:42 +0800

    test: add test for template::apply

--- src/template/apply.rs
+++ src/template/apply.rs
@@ -35,8 +35,9 @@ fn copy_dir(src: &Path, dst: &Path) -> Result<()> {
         if src_path.is_dir() {
             copy_dir(&src_path, &dst_path)?;
         } else {
-            std::fs::copy(&src_path, &dst_path)
+            fs::copy(&src_path, &dst_path)
                 .with_context(|| format!("failed to copy file: {}", src_path.display()))?;
+            // TODO: set permissions
         }
     }
 
@@ -48,12 +49,14 @@ mod tests {
     use super::*;
 
     use std::fs;
+    use std::path::PathBuf;
 
     use hamcrest2::prelude::*;
+    use tempfile::TempDir;
 
     use crate::test_utils::TemplateHome;
 
-    fn setup() -> (TemplateHome, Template) {
+    fn setup_home() -> (TemplateHome, Template) {
         let home = TemplateHome::single("test template", Some(r#"description = "Test""#));
 
         let template_path = home.dirs()[0].path();
@@ -63,22 +66,31 @@ mod tests {
         fs::write(template_dir.join("file.txt"), "Some content").unwrap();
         fs::write(template_dir.join("another_file.txt"), "Ohter content").unwrap();
 
+        fs::create_dir(template_dir.join("dir")).unwrap();
+        fs::write(template_dir.join("dir").join("file.txt"), "Some content").unwrap();
+
         let template = Template::load(template_path).unwrap();
 
         (home, template)
     }
 
-    #[test]
-    fn it_works() {
-        let (_home, template) = setup();
-
+    fn setup_target() -> (TempDir, PathBuf) {
         let temp_dir = tempfile::tempdir().unwrap();
         let target_path = temp_dir.path().join("target");
+        (temp_dir, target_path)
+    }
+
+    #[test]
+    fn it_works() {
+        let (_home, template) = setup_home();
+        let (_temp_dir, target_path) = setup_target();
         template.apply(&target_path).unwrap();
 
         assert_that!(&target_path, path_exists());
         assert_that!(&target_path.join("file.txt"), file_exists());
         assert_that!(&target_path.join("another_file.txt"), file_exists());
+        assert_that!(&target_path.join("dir"), path_exists());
+        assert_that!(&target_path.join("dir").join("file.txt"), file_exists());
 
         assert_eq!(
             fs::read_to_string(target_path.join("file.txt")).unwrap(),
@@ -88,21 +100,50 @@ mod tests {
             fs::read_to_string(target_path.join("another_file.txt")).unwrap(),
             "Ohter content"
         );
+        assert_eq!(
+            fs::read_to_string(target_path.join("dir").join("file.txt")).unwrap(),
+            "Some content"
+        );
     }
 
-    // TODO:implement the following test
     #[test]
-    fn template_dir_missing() {}
+    fn template_dir_missing() {
+        let home = TemplateHome::single("test template", Some(r#"description = "Test""#));
+        let template = Template::load(home.dirs()[0].path()).unwrap();
+        let (_temp_dir, target_path) = setup_target();
 
-    // TODO:implement the following test
-    #[test]
-    fn template_dir_is_file() {}
+        assert_that!(template.apply(&target_path), err());
+    }
 
-    // TODO:implement the following test
     #[test]
-    fn target_path_is_file() {}
+    fn template_dir_is_file() {
+        let home = TemplateHome::single("test template", Some(r#"description = "Test""#));
+        fs::write(
+            home.dirs()[0].path().join(Template::TEMPLATE_DIR),
+            "some content",
+        )
+        .unwrap();
+        let template = Template::load(home.dirs()[0].path()).unwrap();
+        let (_temp_dir, target_path) = setup_target();
 
-    // TODO:implement the following test
+        assert_that!(template.apply(&target_path), err());
+    }
+
     #[test]
-    fn target_path_already_exists() {}
+    fn target_path_is_file() {
+        let (_home, template) = setup_home();
+        let (_temp_dir, target_path) = setup_target();
+        fs::write(&target_path, "some content").unwrap();
+
+        assert_that!(template.apply(&target_path), err());
+    }
+
+    #[test]
+    fn target_path_already_exists() {
+        let (_home, template) = setup_home();
+        let (_temp_dir, target_path) = setup_target();
+        fs::create_dir(&target_path).unwrap();
+
+        assert_that!(template.apply(&target_path), err());
+    }
 }
